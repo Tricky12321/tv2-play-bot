1243f9d097a6b9faf869617068ada0ea58d72746
Thue  Vestergaard Iversen <tvi@moonskye.com>
2022-05-04 13:06:56 +0200

    Made tv2 remove device bot

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM php:8.0-zts
+
+RUN apt update && apt install -y \
+    htop \
+    zsh \
+    vim \
+    git \
+    lsof \
+    psmisc \
+    zip \
+    libzip-dev \
+    procps \
+    dlang-libevent
+
+RUN mkdir /tv2-play-bot
+WORKDIR /tv2-play-bot
+RUN docker-php-ext-configure pcntl --enable-pcntl
+RUN docker-php-ext-install sockets
+RUN pecl install event
+RUN docker-php-ext-install zip
+RUN docker-php-ext-install pcntl
+RUN pecl install xdebug
+RUN docker-php-ext-enable xdebug
+RUN docker-php-ext-install shmop
+
+
+
+RUN apt update && apt install -y \
+    chromium
+COPY --from=composer /usr/bin/composer /usr/bin/composer
+ADD . /tv2-play-bot
+WORKDIR /tv2-play-bot
+RUN composer up
+VOLUME /tv2-play-bot/storage
+ADD src /tv2-play-bot/src
+CMD php src/main.php
---
+++ src/main.php
@@ -0,0 +1,42 @@
+<?php
+require "../vendor/autoload.php";
+
+use HeadlessChromium\BrowserFactory;
+
+$browserFactory = new BrowserFactory();
+while (true) {
+    $browser = $browserFactory->createBrowser(["noSandbox" => true]);
+    $credentials = include "config.php";
+    try {
+        $page = $browser->createPage();
+        $page->navigate('https://mit.tv2.dk/konto/play/enheder')->waitForNavigation();
+        $pageTitle = $page->evaluate('document.title')->getReturnValue();
+        if ($pageTitle == "Log ind") {
+            echo "Not logged in, logging in...\n";
+            // Username
+            $page->evaluate('document.querySelector("#input-guid-1").focus();');
+            $page->keyboard()->typeText($credentials["username"]);
+            $page->keyboard()->typeRawKey('Tab');
+            $page->keyboard()->typeText($credentials["password"]);
+            $page->keyboard()->typeRawKey('Tab');
+            sleep(2);
+            $page->evaluate('document.querySelector("#app > div > div > div > div.FormContainer_content__4NaEi > form > div > div.Bottom_container__EKoqD > button").click()');
+            sleep(3);
+            echo "Login complete\n";
+        }
+        $numOfDevices = $page->evaluate('document.querySelectorAll(".datatable__tbody > tr").length.toString();')->getReturnValue();
+        while($numOfDevices > 0) {
+            $deviceName = $page->evaluate('document.querySelector(".datatable__tbody > tr > td:nth-child(1) > strong").innerHTML.toString()')->getReturnValue();
+            $page->evaluate('document.querySelector(".datatable__tbody > tr:nth-child(1) > td:nth-child(4) > button").click()');
+            sleep(1);
+            $numOfDevices = $page->evaluate('document.querySelectorAll(".datatable__tbody > tr").length.toString();')->getReturnValue();
+            echo "Removed device $deviceName...\n";
+            sleep(1);
+        }
+        echo "Removed all devices\n";
+    } finally {
+        $browser->close();
+    }
+    sleep(300);
+}
+
